9fb93039128cadc8a9fe7c321aa8a61f7eda5e3d
ArcaneTesla <agronaut73@gmail.com>
2024-05-19 03:43:36 +0300

    fast fix

--- Tesla3d/tsl_model.cpp
+++ Tesla3d/tsl_model.cpp
@@ -31,9 +31,7 @@ namespace tsl {
         createIndexBuffers(builder.indices);
     }
 
-    TslModel::~TslModel() {
-
-    }
+    TslModel::~TslModel() {}
 
     std::unique_ptr<TslModel> TslModel::createModelFromFile(TslDevice& device, const std::string& filepath) {
         Builder builder{};
@@ -84,7 +82,7 @@ namespace tsl {
         TslBuffer stagingBuffer{
             tslDevice,
             indexSize,
-            indexSize,
+            indexCount,
             VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
         };
